71e3392375aa035ae651470c296456767a8d8f57
earuiz1 <eliezer_alex10@hotmail.com>
2021-10-28 18:19:25 -0500

    Logic seems to be working but there are still a few bugs to fix

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-// alert("Working");
 
+//Declare constants
 const submit = document.querySelector('#button-submit');
-
-//Check when the button was clicked
-submit.addEventListener('click', () => {
-
-    console.log("Button was successfuly clicked!");
-    console.log(getRandomUpper() + getRandomLower() + getRandomSymbol() + getRandomNumber());
-
-
-});
+const length = document.querySelector('#password-length');
+const upperCase = document.querySelector('#uppercase');
+const lowerCase = document.querySelector('#lowercase');
+const number = document.querySelector('#numbers');
+const symbol = document.querySelector('#symbols');
+const password = document.querySelector('#password');
 
 //Create a function to get a random lowercase letter 
 function getRandomLower() {
@@ -28,8 +25,68 @@ function getRandomNumber() {
 
 //Create a function to get a random symbol 
 function getRandomSymbol() {
-    let symbols = '!@#$%^&*(){}<>-_='
+    let symbols = '!@#$%^&*(){}<>-_=';
     return symbols[Math.floor(Math.random() * symbols.length)]
 }
+
+//Create a function to generate items when the checkboxes are checked
+function generateItems() {
+    let items = []; 
+
+    //Check if the uppercase box is checked 
+    if(upperCase.checked)
+    {
+        //Push uppercase letter to the array
+        items.push(getRandomUpper());
+    }
+
+    //Check if the lowercase box is checked 
+    if(lowerCase.checked)
+    {
+        //Push lowercase letter to the array
+        items.push(getRandomLower());
+    }
+
+    //Check if the number box is checked 
+    if(number.checked)
+    {
+        //Push number to the array
+        items.push(getRandomNumber());
+    }
+
+    //Check if the symbol box is checked 
+    if(symbol.checked)
+    {
+        //Push symbol to the array
+        items.push(getRandomSymbol());
+    }
+
+    return items[Math.floor(Math.random() * items.length)];
+}
+
+
+//Create a function to generate random password
+function generatePassword() {
+    let len = length.value;
+    let pass = '';
+
+    for(let i=0; i< len; i++){
+        pass += generateItems();
+    }
+
+    password.value = pass;
+}
+
+//Check when the button was clicked
+submit.addEventListener('click', () => {
+    //console.log("Button was clicked!");
+    generatePassword();
+});
+
+
+
+
+
+
 
 
